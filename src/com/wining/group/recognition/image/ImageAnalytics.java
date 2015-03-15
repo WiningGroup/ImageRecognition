f954d1a1806719e4ac32868b3b81350ef65b260f
Administrator <Administrator@kangjun-PC>
2015-03-15 16:55:25 +0800

    增加图片处理类

---
+++ src/com/wining/group/recognition/image/ImageAnalytics.java
@@ -0,0 +1,13 @@
+package com.wining.group.recognition.image;
+
+import org.opencv.core.Mat;
+
+public class ImageAnalytics {
+	public ImageAnalytics() {
+
+	}
+
+	public void imageCv(String fileName) {
+
+	}
+}
