8c239091f122fce0ac05c3e50bb67414622023d7
FengZhe <FengZhe@FengZhe-PC>
2015-03-15 18:14:19 -0700

    设置列表适配器 GridView->ListView

--- src/com/wining/group/recognition/activity/MainActivity.java
+++ src/com/wining/group/recognition/activity/MainActivity.java
@@ -2,12 +2,9 @@ package com.wining.group.recognition.activity;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
@@ -15,11 +12,15 @@ import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
-import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.wining.group.recognition.R;
 import com.wining.group.recognition.base.BaseActivity;
+import com.wining.group.recognition.base.ImageItemAdapter;
+import com.wining.group.recognition.entity.ImageEntity;
+import com.wining.group.recognition.image.ImageAnalytics;
+import com.wining.group.recognition.image.ImageAnalytics.IOnResutlListener;
 
 public class MainActivity extends BaseActivity {
 
@@ -27,13 +28,13 @@ public class MainActivity extends BaseActivity {
 
 	private Button mBtnTransferCamera, mBtnFinishedOrder;
 
-	private List<Bitmap> mBitmapList;
+	private List<ImageEntity> mResultImagesList;
 
-	private GridView mGridView;
-
-	private List<Map<String, Object>> mGridViewDatas;
+	private ListView mImagesListView;
 
 	private File fileToPhoto;
+
+	private ImageItemAdapter mAdapter;
 
 	/**
 	 * @method openCamera
@@ -47,27 +48,19 @@ public class MainActivity extends BaseActivity {
 	}
 
 	private void refreshImageBuffer() {
-		mGridViewDatas.clear();
-		for (int i = 0; i < mBitmapList.size(); i++) {
-			HashMap<String, Object> map = new HashMap<String, Object>();
-			map.put("photo", mBitmapList.get(i));
-			mGridViewDatas.add(map);
-		}
 	}
 
 	@Override
 	protected void initComponent() {
 		// TODO Auto-generated method stub
 
-		mBitmapList = new ArrayList<Bitmap>();
-
-		mGridViewDatas = new ArrayList<Map<String, Object>>();
+		mResultImagesList = new ArrayList<ImageEntity>();
 
 		mBtnTransferCamera = (Button) findViewById(R.id.btn_transfer_system_camera);
 
 		mBtnFinishedOrder = (Button) findViewById(R.id.btn_finished_order);
 
-		mGridView = (GridView) findViewById(R.id.photoGridview);
+		mImagesListView = (ListView) findViewById(R.id.photoListview);
 	}
 
 	@Override
@@ -91,11 +84,10 @@ public class MainActivity extends BaseActivity {
 
 			@Override
 			public void onClick(View v) {
-
 			}
 		});
 
-		mGridView.setOnItemClickListener(new OnItemClickListener() {
+		mImagesListView.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -123,9 +115,18 @@ public class MainActivity extends BaseActivity {
 		super.onActivityResult(requestCode, resultCode, data);
 
 		if (resultCode == RESULT_OK) {
-			Bitmap bmp = (Bitmap) data.getExtras().get("data");
-			mBitmapList.add(bmp);
-			refreshImageBuffer();
+
+			new ImageAnalytics().setIOnResutlListener(new IOnResutlListener() {
+
+				@Override
+				public void onResule(ImageEntity imageEntity) {
+					mResultImagesList.add(imageEntity);
+					mAdapter = new ImageItemAdapter(mResultImagesList);
+					mImagesListView.setAdapter(mAdapter);
+					mAdapter.notifyDataSetChanged();
+				}
+			});
+
 		}
 
 	}
--- src/com/wining/group/recognition/image/ImageAnalytics.java
+++ src/com/wining/group/recognition/image/ImageAnalytics.java
@@ -18,7 +18,7 @@ public class ImageAnalytics {
 		}
 	}
 
-	interface IOnResutlListener {
+	public interface IOnResutlListener {
 		void onResule(ImageEntity imageEntity);
 	}
 
